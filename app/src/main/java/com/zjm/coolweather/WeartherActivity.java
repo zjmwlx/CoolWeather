2566dc2cc36e07896d66843bddd47e6678528874
zjm <zjmwlx@126.com>
2017-07-02 16:32:37 +0800

    第6次上传，完成标题栏的优化

--- app/src/main/java/com/zjm/coolweather/WeartherActivity.java
+++ app/src/main/java/com/zjm/coolweather/WeartherActivity.java
@@ -1,6 +1,8 @@
 package com.zjm.coolweather;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -45,6 +47,11 @@ public class WeartherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wearther);
+        if (Build.VERSION.SDK_INT>=21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         /**
          * 初始化控件
          */
